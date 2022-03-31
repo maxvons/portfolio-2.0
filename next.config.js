32ae39598fddb02dd848c855b99cf1fbb5c1d7b1
Maximilian von Stephanides <maximilianvons@gmail.com>
2022-03-31 18:15:06 +0200

    📦 NEW: Setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "variables"; @import "mixins";`,
+  }
 }
 
 module.exports = nextConfig
