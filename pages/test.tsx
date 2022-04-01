99f17ebde534f0ff7676301109229c017eba47e6
Maximilian von Stephanides <maximilianvons@gmail.com>
2022-04-01 14:58:27 +0200

    📦 NEW: Add test page

---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+import { NextPage } from "next";
+
+const Test: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Test</h1>
+    </Layout>
+  );
+};
+
+export default Test;
