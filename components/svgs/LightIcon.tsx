b8ee598e00b9232017e3980139233d7dd6355fdc
Maximilian von Stephanides <maximilianvons@gmail.com>
2022-04-05 00:29:56 +0200

    👌 IMPROVE: Standardize dark- and light icon
    - Only import 24x24 icons for consistency.

--- components/svgs/LightIcon.tsx
+++ components/svgs/LightIcon.tsx
@@ -6,48 +6,41 @@ const LightIcon = ({ className }: LightIconProps) => {
   return (
     <svg
       className={className}
-      width="18"
-      height="18"
-      viewBox="0 0 18 18"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clipPath="url(#clip0_229_6309)">
-        <path
-          d="M9 12.75C11.0711 12.75 12.75 11.0711 12.75 9C12.75 6.92893 11.0711 5.25 9 5.25C6.92893 5.25 5.25 6.92893 5.25 9C5.25 11.0711 6.92893 12.75 9 12.75Z"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path d="M9 0.75V2.25" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M9 15.75V17.25" strokeLinecap="round" strokeLinejoin="round" />
-        <path
-          d="M3.16602 3.16504L4.23102 4.23004"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M13.7695 13.77L14.8345 14.835"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path d="M0.75 9H2.25" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M15.75 9H17.25" strokeLinecap="round" strokeLinejoin="round" />
-        <path
-          d="M3.16406 14.835L4.22906 13.77"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M13.7695 4.23004L14.8345 3.16504"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_229_6309">
-          <rect width="18" height="18" fill="white" />
-        </clipPath>
-      </defs>
+      <path
+        d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M12 1V3" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12 21V23" strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M4.2207 4.21997L5.6407 5.63997"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.3594 18.3601L19.7794 19.7801"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M1 12H3" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M21 12H23" strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        d="M4.21875 19.7801L5.63875 18.3601"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.3594 5.63997L19.7794 4.21997"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 };
