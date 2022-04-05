1a0ef895e3431770c0d8c3710958b2fe0663c0b9
Maximilian von Stephanides <maximilianvons@gmail.com>
2022-04-05 16:20:08 +0200

    👌 IMPROVE: Wrap landing component in layout
    - Wrap landing component in layout component.
      - To enable more flexbility and styling possibilities.

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -2,6 +2,7 @@
 import CustomImage from "./CustomImage";
 import Button from "./Button";
 import Toolbar from "./ToolBar";
+import Layout from "./Layout";
 
 /* Assets. */
 import Portrait from "/public/images/portrait.jpg";
@@ -14,28 +15,34 @@ import styles from "../styles/Landing.module.scss";
 
 const Landing = () => {
   return (
-    <div className={styles.container}>
-      <p className={styles.tag}>My name is</p>
-      <h1 className={styles.title}>Maximilian</h1>
-      <h2 className={styles.subTitle}>
-        I create <span className={styles.flare}>exceptional</span> digital
-        experiences.
-      </h2>
-      <Button text="Grab my resume" href="/resume" className={styles.button} />
-      <CustomImage
-        src={Portrait}
-        alt="Portrait of Maximilian"
-        width={2052}
-        height={3078}
-        objectPosition="50% 20%"
-        size={ImageSize.AVATAR}
-        round
-        className={styles.image}
-      />
-      <div className={styles.toolbar}>
-        <Toolbar />
+    <Layout>
+      <div className={`${styles.container} ${styles.marginTop}`}>
+        <p className={styles.tag}>My name is</p>
+        <h1 className={styles.title}>Maximilian</h1>
+        <h2 className={styles.subTitle}>
+          I create <span className={styles.flare}>exceptional</span> digital
+          experiences.
+        </h2>
+        <Button
+          text="Grab my resume"
+          href="/resume"
+          className={styles.button}
+        />
+        <CustomImage
+          src={Portrait}
+          alt="Portrait of Maximilian"
+          width={2052}
+          height={3078}
+          objectPosition="50% 20%"
+          size={ImageSize.AVATAR}
+          round
+          className={styles.image}
+        />
+        <div className={styles.toolbar}>
+          <Toolbar />
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
