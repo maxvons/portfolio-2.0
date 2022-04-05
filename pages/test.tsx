869eecaa2afcc39d6fe99d0f878d102ebb2d8fd7
Maximilian von Stephanides <maximilianvons@gmail.com>
2022-04-05 11:01:55 +0200

    📦 NEW: Add example usage of image component

--- pages/test.tsx
+++ pages/test.tsx
@@ -7,11 +7,15 @@ import LightIcon from "../components/svgs/LightIcon";
 import { NextPage } from "next";
 import { useState } from "react";
 
+import { ImageSize } from "../types/types";
+
 import styles from "../styles/Test.module.scss";
 import DarkIcon from "../components/svgs/DarkIcon";
 import IconLink from "../components/IconLink";
 import GitHubIcon from "../components/svgs/GitHubIcon";
 import Toolbar from "../components/ToolBar";
+import CustomImage from "../components/CustomImage";
+import Portrait from "/public/images/portrait.jpg";
 
 const Test: NextPage = () => {
   const [open, setOpen] = useState(false);
@@ -32,6 +36,15 @@ const Test: NextPage = () => {
         />
         <IconLink href="https://www.google.com" Icon={GitHubIcon} />
         <Toolbar dark />
+        <CustomImage
+          src={Portrait}
+          alt="Portrait of Maximilian"
+          width={2052}
+          height={3078}
+          objectPosition="50% 20%"
+          size={ImageSize.AVATAR}
+          round
+        />
         <h1>Test</h1>
         <h1>Test</h1>
         <h1>Test</h1>
