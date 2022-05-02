db9e61ca96c6b804b1e6c90bfb7f7476f699ee71
Maximilian von Stephanides <maximilianvons@gmail.com>
2022-05-02 16:41:20 +0200

    📦 NEW: Add timeline component

---
+++ components/Timeline.tsx
@@ -0,0 +1,50 @@
+// Next.js.
+import Image from "next/image";
+
+// Types.
+import { TimelinePoint } from "../types/types";
+
+// Styles.
+import styles from "../styles/Timeline.module.scss";
+
+interface TimelineProps {
+  timelinePoints: TimelinePoint[];
+}
+
+const Timeline = ({ timelinePoints }: TimelineProps) => {
+  return (
+    <div className={styles.points}>
+      {timelinePoints.map((point, index) => (
+        <div key={index} className={styles.point}>
+          <div className={styles.dateAndLocation}>
+            <p className={styles.location}>{point.location}</p>
+            <p className={styles.date}>{point.date}</p>
+          </div>
+          <div className={styles.connectorAndCircle}>
+            <span className={styles.circle} />
+            <span className={styles.connector} />
+          </div>
+          <div className={styles.titleAndDescription}>
+            <h3 className={styles.title}>{point.title}</h3>
+            <p className={styles.description}>{point.description}</p>
+          </div>
+          {point.image && (
+            <div className={styles.imageContainer}>
+              <Image
+                src={point.image.src}
+                alt={point.image.alt}
+                width={point.image.width}
+                height={point.image.height}
+                placeholder="blur"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Timeline;
