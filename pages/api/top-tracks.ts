a258915dcd3bdac4d636be1ce960dec88ee10ba7
Maximilian von Stephanides <maximilianvons@gmail.com>
2023-06-11 18:04:08 +0200

    Update top tracks endpoint to include blurhash string

--- pages/api/top-tracks.ts
+++ pages/api/top-tracks.ts
@@ -1,26 +1,48 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getTopTracks } from "../../utils/spotify";
+import sharp from "sharp";
+import { encode } from "blurhash";
+import { SpotifyAlbum } from "../../types/types";
 
 type Data = {
-  artist: string;
+  id: string;
+  artists: string[];
   songUrl: string;
   title: string;
+  album: SpotifyAlbum;
+  blurhash: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data[]>,
 ) {
   const response = await getTopTracks().then((res) => res.json());
-  const { items } = await response;
+  const { items: tracks } = await response;
 
-  const topTenTracks = items.slice(0, 10).map((track: any) => ({
-    id: track.id,
-    artists: track.artists,
-    songUrl: track.external_urls.spotify,
-    title: track.name,
-    album: track.album,
-  }));
+  const topTenTracksWithBlurHash = await Promise.all(
+    tracks.map(async (track: any) => {
+      const imageResponse = await fetch(track.album.images[0].url);
+      const arrayBuffer = await imageResponse.arrayBuffer();
 
-  return res.status(200).json(topTenTracks);
+      const pixels = await sharp(arrayBuffer)
+        .resize(32, 32)
+        .raw()
+        .ensureAlpha()
+        .toBuffer();
+      const clamped = new Uint8ClampedArray(pixels);
+      const blurhash = encode(clamped, 32, 32, 4, 4);
+
+      return {
+        id: track.id,
+        artists: track.artists,
+        songUrl: track.external_urls.spotify,
+        title: track.name,
+        album: track.album,
+        blurhash: blurhash,
+      };
+    }),
+  );
+
+  return res.status(200).json(topTenTracksWithBlurHash);
 }
