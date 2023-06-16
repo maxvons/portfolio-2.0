83ef07fb04606d613d087715a5debcec84ee028d
Maximilian von Stephanides <maximilianvons@gmail.com>
2023-06-16 17:59:27 +0200

    Update the NowPlaying component to support the new features

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -3,8 +3,10 @@ import fetcher from "../utils/fetcher";
 import { deserializeNowPlaying, renderArtists } from "../utils/functions";
 import Spotify from "./svgs/Spotify";
 import styles from "../styles/NowPlaying.module.scss";
-import { Episode, Song } from "../types/types";
+import { Song } from "../types/types";
 import ProgressBar from "./ProgressBar";
+import MusicBars from "./MusicBars";
+import PauseIcon from "./svgs/PauseIcon";
 
 const NowPlaying = () => {
   const { data, error, isLoading } = useSWR("/api/currently-playing", fetcher);
@@ -25,40 +27,71 @@ const NowPlaying = () => {
     );
   }
 
-  if (error) {
-    return (
-      <div className={styles.container}>
-        <p className={styles.title}>Not playing</p>
-        <div className={styles.data}>
-          <Spotify className={styles.icon} />
-          <div className={styles.songData}>
-            <p className={styles.songName}>-</p>
-            <p className={styles.artists}>-</p>
-          </div>
-        </div>
-        <ProgressBar progress_ms={0} total_ms={0} noFill />
-      </div>
-    );
+  if (error || !data) {
+    renderNotPlaying();
   }
 
   // Render data from Spotify API.
   const nowPlaying = deserializeNowPlaying(data);
+  if ("artists" in nowPlaying) {
+    return renderSong(nowPlaying);
+  } else {
+    return renderNotPlaying();
+  }
+};
+
+function renderSong(song: Song) {
   return (
     <div className={styles.container}>
-      <p className={styles.title}>Now Playing</p>
+      {renderTitle(song.is_playing)}
       <div className={styles.data}>
         <Spotify className={styles.icon} />
         <div className={styles.songData}>
-          <p className={styles.songName}>{nowPlaying.name}</p>
-          <p className={styles.artists}>{renderArtists(nowPlaying)}</p>
+          <p className={styles.songName}>{song.name}</p>
+          <p className={styles.artists}>{renderArtists(song)}</p>
         </div>
       </div>
       <ProgressBar
-        progress_ms={nowPlaying.progress_ms ? nowPlaying.progress_ms : 0}
-        total_ms={nowPlaying.duration_ms ? nowPlaying.duration_ms : 0}
+        progress_ms={song.progress_ms ? song.progress_ms : 0}
+        total_ms={song.duration_ms ? song.duration_ms : 0}
+        playing={song.is_playing}
       />
     </div>
   );
-};
+}
+
+function renderTitle(playing?: boolean) {
+  if (playing) {
+    return (
+      <div className={styles.titleContainer}>
+        <MusicBars />
+        <p className={styles.title}>Now Playing</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={`${styles.titleContainer} ${styles.paused}`}>
+      <PauseIcon className={styles.pauseIcon} />
+      <p className={styles.title}>Paused</p>
+    </div>
+  );
+}
+
+function renderNotPlaying() {
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>Not playing</p>
+      <div className={styles.data}>
+        <Spotify className={styles.icon} />
+        <div className={styles.songData}>
+          <p className={styles.songName}>-</p>
+          <p className={styles.artists}>-</p>
+        </div>
+      </div>
+      <ProgressBar progress_ms={0} total_ms={0} noFill />
+    </div>
+  );
+}
 
 export default NowPlaying;
